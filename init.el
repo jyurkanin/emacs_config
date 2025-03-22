fada67d27566797493b51784a1aa4723fef9d418
yurkanin <yurkanin321@gmail.com>
2025-03-22 17:31:09 -0500

    I have created perhaps the greatest C++ emacs config of all time. It is almost close to being perfect

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+;;; TODO: Install an lsp for python
+;;; TODO: dired-find-alternate-file
+
+
+
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/")
@@ -14,6 +19,15 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+(use-package company
+  :ensure t
+  :bind
+  ( :map company-mode-map
+    ("M-s /" . company-complete))
+  :config
+  (setq company-idle-delay 100)
+  )
 
 (use-package dired
   :ensure nil
@@ -41,6 +55,14 @@
   :ensure t
   :config
   (require 'dap-cpptools)
+  )
+
+(use-package lsp-ui
+  :after lsp-mode
+  :ensure t
+  :config
+  (setq lsp-ui-doc-enable t
+        lsp-ui-sideline-enable nil)
   )
 
 (use-package dap-mode
@@ -101,7 +123,7 @@
   (require 'llm-ollama)
   (setq ellama-provider
           (make-llm-ollama
-           :chat-model "llama3";"llama3:8b-instruct-q8_0"
+           :chat-model "phi4"
            :embedding-model "nomic-embed-text"))
   )
 
@@ -131,6 +153,38 @@ The DWIM behaviour of this command is as follows:
 
 (define-key global-map (kbd "C-g") #'prot/keyboard-quit-dwim)
 
+(defun open-init-file ()
+  "Open the Emacs configuration file."
+  (interactive)
+  (find-file user-init-file))
+
+(defun project-find-file-at-point ()
+  "Search for a file in the current project matching the filename at point."
+  (interactive)
+  (let* ((project (project-current))
+         (root (when project (project-root project)))
+         (filename (substring-no-properties (thing-at-point 'filename)))
+         (files (when project (project-files project)))
+         (matches (when (and filename (not (string-empty-p filename)))
+                    (seq-filter (lambda (f) (string-match-p (regexp-quote filename) f)) files))))
+    (cond
+     ((not project) (message "Not in a project."))
+     ((not filename) (message "No filename found at point."))
+     ((null matches) (message "No matching file found for: %s" filename))
+     ((= (length matches) 1) (find-file (expand-file-name (car matches) root)))
+     (t (find-file (expand-file-name (completing-read "Select file: " matches) root))))))
+
+(defun copy-symbol-at-point ()
+  (interactive)
+  (kill-new (thing-at-point 'symbol)))
+
+(define-key global-map (kbd "M-s c") #'open-init-file)
+(define-key global-map (kbd "C-x b") #'counsel-switch-buffer)
+(define-key global-map (kbd "M-s s") #'counsel-at-point-rg)
+(define-key global-map (kbd "M-s f") #'project-find-file-at-point)
+(define-key global-map (kbd "M-s w") #'copy-symbol-at-point)
+(define-key global-map (kbd "M-s S") #'counsel-rg)
+(define-key global-map (kbd "M-s d") #'lsp-ui-doc-show)
 
 (defun my-c++-mode-hook ()
   (c-set-offset 'substatement-open 0)
@@ -141,6 +195,8 @@ The DWIM behaviour of this command is as follows:
   (setq tab-width 4)
   (setq indent-tabs-mode nil)
   (local-set-key (kbd "C-c o") 'ff-find-other-file)
+  (lsp)
+  (lsp-ui-mode)
   )
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
@@ -152,6 +208,7 @@ The DWIM behaviour of this command is as follows:
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.cuh\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cpp\\'" . c++-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -171,12 +228,13 @@ The DWIM behaviour of this command is as follows:
  '(package-selected-packages
    '(ace-window achievements all-the-icons anzu auctex blamer
                 clean-aindent-mode cmake-mode company
-                company-c-headers compat dap-mode dired-subtree
-                doom-modeline dtrt-indent ellama evil git god-mode
-                golden-ratio google-this magit orderless prescient
-                solaire-mode spacemacs-theme tree-sitter
-                tree-sitter-langs undo-tree use-package vertico
-                vertico-prescient which-key zygospore))
+                company-c-headers compat consult counsel-at-point
+                dap-mode dired-subtree doom-modeline dtrt-indent
+                ellama evil git god-mode golden-ratio google-this
+                gptel lsp-ui magit orderless prescient projectile
+                pyvenv ripgrep solaire-mode spacemacs-theme
+                tree-sitter tree-sitter-langs undo-tree use-package
+                vertico vertico-prescient which-key zygospore))
  '(truncate-lines t)
  '(window-divider-default-right-width 18))
 
@@ -213,7 +271,7 @@ The DWIM behaviour of this command is as follows:
 
 (setq buffer-age-limit 1800)
 (setq inhibit-startup-screen t)
-
+(setq ring-bell-function (lambda (&rest _) )) ; Disable the stupid sound effect on error.
 
 (set-face-foreground 'vertical-border (if (true-color-p) "#292b2e" "#262626"))
 (set-face-attribute  'mode-line          nil :box nil)
@@ -229,3 +287,20 @@ The DWIM behaviour of this command is as follows:
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'erase-buffer 'disabled nil)
+
+(defvar parameters
+  '(window-parameters . ((no-other-window . t)
+                         (no-delete-other-windows . t))))
+(defun dired-left ()
+  "Display default directory on left"
+  (interactive)
+  (let ((buffer (dired-noselect "/home/justin")))
+    (with-current-buffer buffer (dired-hide-details-mode t))
+    (display-buffer-in-side-window
+     buffer `((side . left)
+              (slot  . 0)
+              (window-width . 40)
+              (preserve-size . (t. nil)), parameters))
+    ))
+(put 'dired-find-alternate-file 'disabled nil)
